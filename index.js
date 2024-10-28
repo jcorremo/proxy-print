9a37ed315a2359a15f17a27c6d8cff324124fc0f
Jaime Correa <jcorremo@coode.tech>
2024-10-27 20:30:16 -0500

    Add PATH

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var bodyParser = require('body-parser')
 const requestListener = function (req, res) {
 
   console.log(`REQUEST HEADER:: ${JSON.stringify(req.headers)}`)
-
+  console.log(`REQUEST URL:: ${req.url}`)
+  console.log(`REQUEST METHOD:: ${req.method}`)
+  
   req.on('data', chunk => {
     console.log(`REQUEST BODY:: ${chunk}`)
   })
