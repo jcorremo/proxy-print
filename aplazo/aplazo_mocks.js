298d5b2493e17bb766b82ae2e23e5c94794a3cb4
Jaime Correa <jaime.correa@aplazo.mx>
2025-08-12 15:20:20 -0500

    Add mock for aplazo

---
+++ aplazo/aplazo_mocks.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto');
+
+function randomString() {
+  return crypto.randomUUID();
+}
+
+module.exports = function aplazoMocks(req, res) {
+  if (req.method === 'GET' && req.path === '/wm/callback') {
+    const response = {
+      creditLineId: randomString()
+    };
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    return res.end(JSON.stringify(response));
+  }
+
+  res.writeHead(200);
+  return res.end();
+};
--- index.js
+++ index.js
@@ -1,29 +1,34 @@
+
 const http = require("http");
 const host = '0.0.0.0';
 const port = 8080;
-
-var bodyParser = require('body-parser')
+const aplazoMocks = require('./aplazo/aplazo_mocks');
 
 const requestListener = function (req, res) {
+  console.log(`REQUEST HEADER:: ${JSON.stringify(req.headers)}`);
+  console.log(`REQUEST URL:: ${req.url}`);
+  console.log(`REQUEST METHOD:: ${req.method}`);
 
-  console.log(`REQUEST HEADER:: ${JSON.stringify(req.headers)}`)
-  console.log(`REQUEST URL:: ${req.url}`)
-  console.log(`REQUEST METHOD:: ${req.method}`)
-  
+  let body = '';
   req.on('data', chunk => {
-    console.log(`REQUEST BODY:: ${chunk}`)
-  })
+    body += chunk;
+    console.log(`REQUEST BODY:: ${chunk}`);
+  });
 
   req.on('end', () => {
-	console.log('No more data');  	
-  	res.end();
-  })
-
+    console.log('No more data');
+    if (req.url.startsWith('/aplazo')) {
+      req.path = req.url.replace('/aplazo', '');
+      return aplazoMocks(req, res);
+    }
+    res.end();
+  });
 };
 
 const server = http.createServer(requestListener);
+
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
 
 
